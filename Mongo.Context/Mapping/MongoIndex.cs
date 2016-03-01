01c32b4de800559e0d1daa45ca87156c737a45fb
James Canady <jcanady20@gmail.com>
2016-02-29 18:14:30 -0600

    Added Default value to TimeToLive value = -1

--- Mongo.Context/Mapping/MongoIndex.cs
+++ Mongo.Context/Mapping/MongoIndex.cs
@@ -8,6 +8,7 @@ namespace Mongo.Context.Mapping
         public MongoIndex()
         {
             Keys = new List<string>();
+            TimeToLive = -1;
         }
         public ICollection<string> Keys { get; set; }
         public bool Desending { get; set; }
